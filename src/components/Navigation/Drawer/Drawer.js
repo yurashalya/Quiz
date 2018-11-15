2d40df40560e9b4e39b7da8caa680e7d6ddf68b3
Yura Shalya <stallker9696@gmail.com>
2018-11-16 00:46:18 +0200

    Menu shadow / new component Backdrop

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import classes from './Drawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 
 
 const links = [
@@ -24,11 +25,14 @@ class Drawer extends Component {
             cls.push(classes.close )
         }
         return (
-            <nav className={cls.join(' ')}>
-                <ul>
-                    { this.renderLinks() }
-                </ul>
-            </nav>
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        { this.renderLinks() }
+                    </ul>
+                </nav>
+                {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null }
+            </React.Fragment>
         )
     }
 }
