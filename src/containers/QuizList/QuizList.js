72fac3fb1ac8ced1c27dd94898230c23ddecda91
Yura Shalya <stallker9696@gmail.com>
2018-11-20 01:58:07 +0200

    Style pages list

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-
+import classes from './QuizList.css'
+import { NavLink } from 'react-router-dom'
 export default class QuizList extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Quiz List</h1>
-      </div>
-    )
-  }
+
+    renderQuizes() {
+        return [1,2,3].map((quiz, index) => {
+            return (
+                <li key = {index}>
+                    <NavLink to={'/quiz/' + quiz}>
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+    render() {
+        return (
+        <div className={classes.QuizList}>
+            <div>
+                <h1>Список тестов</h1>
+                <ul>
+                    {this.renderQuizes()}
+                </ul>
+            </div>
+        </div>
+        )
+    }
 }
